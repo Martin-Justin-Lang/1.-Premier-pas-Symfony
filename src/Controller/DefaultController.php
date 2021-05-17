644849cb473db7a264ee90cf36a65fc507809900
Martin-Justin-Lang <martin.lang67@gmail.com>
2021-05-17 23:50:05 +0200

    Quete 3 done

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,20 @@
+<?php
+// src/Controller/ProgramController.php
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+Class DefaultController extends AbstractController
+{
+    /**
+     * @route("/", name="app_index")
+     */
+    public function index(): Response
+    {
+        return $this->render('default/index.html.twig', [
+            'website' => 'Wild Séries',
+        ]);
+    }
+}
